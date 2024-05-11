492b3288b0042fe392a755330b6c6c9e0ddaccd5
meital.kozhidov <meital.kozhidov@infinitylabs.co.il>
2024-05-11 20:48:23 +0300

    separated to files

---
+++ src/test/java/com/sysaid/assignment/UserControllerTests.java
@@ -0,0 +1,39 @@
+package com.sysaid.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+/******************************************************************************/
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test 
+    void testCreateUser() throws Exception {
+        String username = "meital";
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/users")
+                .param("username", username)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/users")
+                .param("username", username)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isConflict());
+    }
+}
